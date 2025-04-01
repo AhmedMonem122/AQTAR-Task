b715aca5c9eb200029e30401c3b867e375102dbd
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-04-01 16:23:32 +0200

    feat: add add product api route

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -24,3 +24,37 @@ export const GET = async () => {
     });
   }
 };
+
+export const POST = async (request: Request) => {
+  try {
+    const product = await request.json();
+    
+    const { data } = await fakeStoreServer.post("/products", product);
+    
+    return new Response(
+      JSON.stringify({
+        message: "Product added successfully",
+        product: data
+      }), 
+      {
+        status: 201,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    return new Response(
+      JSON.stringify({ 
+        error: axiosError.message 
+      }), 
+      {
+        status: axiosError.response?.status || 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+};
