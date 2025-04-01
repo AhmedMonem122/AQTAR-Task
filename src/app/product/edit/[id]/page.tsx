16e6385cbb6df6022f6808e6b42f3baed3b40e07
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-04-01 17:18:31 +0200

    feat: add edit product page

---
+++ src/app/product/edit/[id]/page.tsx
@@ -0,0 +1,39 @@
+import AddEditProduct from "@/components/AddEditProduct/AddEditProduct";
+import { localServer } from "@/lib/axios-server";
+import { AxiosError } from "axios";
+
+interface ErrorResponse {
+  message: string;
+}
+
+export const dynamic = "force-dynamic";
+
+const EditProductPage = async ({
+  params,
+}: {
+  params: Promise<{
+    id: string;
+  }>;
+}) => {
+  const { id } = await params;
+
+  try {
+    const {
+      data: { product },
+    } = await localServer.get(`/api/products/${id}`);
+
+    return <AddEditProduct product={product} isEditing />;
+  } catch (error) {
+    const axiosError = error as AxiosError<ErrorResponse>;
+    return (
+      <div className="container mx-auto px-4 py-16 text-center">
+        <h2 className="text-2xl font-bold text-red-600">
+          {axiosError.response?.data?.message || axiosError.message}
+        </h2>
+        <p className="mt-2 text-gray-600">Please try again later</p>
+      </div>
+    );
+  }
+};
+
+export default EditProductPage;
