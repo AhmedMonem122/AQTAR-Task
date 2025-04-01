8b6b06363f0ec0c67fb34a4d096d4c71b0dcebb7
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-04-01 16:31:05 +0200

    feat: add delete product api route

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -79,3 +79,45 @@ export const PUT = async (
     );
   }
 };
+
+export const DELETE = async (
+  _request: Request,
+  {
+    params,
+  }: {
+    params: Promise<{
+      id: string;
+    }>;
+  }
+) => {
+  try {
+    const { id } = await params;
+
+    await fakeStoreServer.delete(`/products/${id}`);
+
+    return new Response(
+      JSON.stringify({
+        message: "Product deleted successfully",
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    return new Response(
+      JSON.stringify({
+        error: axiosError.message,
+      }),
+      {
+        status: axiosError.response?.status || 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+};
